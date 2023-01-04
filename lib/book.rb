447abbb66d6833685f8c6c9a6e363d13180f8786
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-04 11:03:12 +0000

    Added definition of equality to book class

--- lib/book.rb
+++ lib/book.rb
@@ -5,4 +5,9 @@ class Book
     @id = id ; @title = title ; @author_name = author_name
   end
   
+  def ==(other)
+    [@id, @title, @author_name] == [
+    other.id, other.title, other.author_name]
+  end
+
 end
