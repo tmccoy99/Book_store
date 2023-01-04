933c17b5669082494f9aee64a2a8d87bc96c4f8e
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-04 11:10:40 +0000

    Implemented #all method

--- lib/book_repository.rb
+++ lib/book_repository.rb
@@ -1,7 +1,15 @@
+require_relative "database_connection.rb"
+require_relative "book.rb"
+
 class BookRepository
 
   def all
-
+    query = "SELECT * FROM books"
+    hashes = DatabaseConnection.exec_params(query, [])
+    books = hashes.map do |hash|
+      Book.new(hash["id"], hash["title"], hash["author_name"])
+    end
+    books
   end
 
   def update
