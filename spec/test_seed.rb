7c2e414c569548891a7a04107a3d7a5b7b576a17
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-04 10:45:48 +0000

    Created SQL seed for testing purposes

---
+++ spec/test_seed.rb
@@ -0,0 +1,4 @@
+TRUNCATE TABLE books RESTART IDENTITY;
+
+INSERT INTO books (title, author_name) VALUES ('Nineteen Eighty-Four', 'George Orwell');
+INSERT INTO books (title, author_name) VALUES ('Mrs Dalloway', 'Virginia Woolf');
