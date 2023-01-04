d49c6ad6879ba1721cce508e3399ef486c2eae3c
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-04 11:03:47 +0000

    Created spec file with test for BookRepository#all method

---
+++ spec/book_repository_spec.rb
@@ -0,0 +1,20 @@
+require "book_repository"
+require "book"
+
+describe BookRepository do
+  
+  before(:each) do
+    @book1 = Book.new("1", "Nineteen Eighty-Four", "George Orwell")
+    @book2 = Book.new("2", "Mrs Dalloway", "Virginia Woolf")
+    @repo = BookRepository.new
+
+    seed = File.read("spec/test_seed.rb")
+    connection = PG.connect({ host: '127.0.0.1',
+    dbname: 'book_store_test'})
+    connection.exec(seed)
+  end
+
+  it "#all returns array of all books" do
+    expect(@repo.all).to eq [@book1, @book2]
+  end
+end
